4f6925f4e405cbb409c1202e0c77bb2127143db2
Isabelle <isabelle.annie.karlsson@gmail.com>
2021-12-07 09:54:28 +0100

    Added html structure

---
+++ blog-client-template/admin/js/create-post.js
@@ -0,0 +1,16 @@
+//Till datumformateringen:
+//let dateAndTimeFunction = (blogPostDate) => {
+    // let dateAndTime = new Date(blogPostDate);
+    //             let year = dateAndTime.getFullYear();
+    //             let month = dateAndTime.getMonth() + 1;
+    //             if (month < 10) {
+    //                 month = `0${month}`;
+    //             }
+    //             let day = dateAndTime.getDate();
+    //             if (day < 10) {
+    //                 day = `0${day}`;
+    //             }
+    //             let formattedDateAndTime = `${year}-${month}-${day} kl ${dateAndTime.getHours()}`;
+//return formattedDateAndTime;
+// }
+//let dateAndTime = dateAndTimeFunction(blogPost.date);
