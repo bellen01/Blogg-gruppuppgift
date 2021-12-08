dbf63b616d2bf156da6d6c9e4cd6d178014ca08f
Isabelle <isabelle.annie.karlsson@gmail.com>
2021-12-08 21:19:26 +0100

    Different solutions for tags function

--- blog-client-template/admin/js/create-post.js
+++ blog-client-template/admin/js/create-post.js
@@ -1,18 +1,19 @@
 window.onload = function () {
     createPostEvent();
+    let title = document.getElementById('title');
+    let author = document.getElementById('author');
+    let content = document.getElementById('content');
+    let displayErrorMessage = document.getElementById('error-messages');
+    let tagsSelect = document.getElementById('tags');
 }
 
-let title = document.getElementById('title');
-let author = document.getElementById('author');
-let content = document.getElementById('content');
-let displayErrorMessage = document.getElementById('error-messages');
-let tagsSelect = document.getElementById('tags');
 
+//flytta till egen js som både jag och Linn kan använda?
 function isStringEmpty(text) {
     return text.trim() === '';
 }
-let errorMessage = '';
 function errorMessages(titleValue, authorValue, contentValue) {
+    let errorMessage = '';
     if (isStringEmpty(titleValue)) {
         errorMessage += 'Title is required<br>';
     }
@@ -22,51 +23,54 @@ function errorMessages(titleValue, authorValue, contentValue) {
     if (isStringEmpty(contentValue)) {
         errorMessage += 'Content is required<br>';
     }
+    return errorMessage;
 }
 
+
+//flytta till egen js som både jag och Linn kan använda?
 let tags = ["Politics", "Satire", "Autobiography", "Lorem Gang"];
-let tagsHTML = '';
-//bryt ut till funktion
-function createTagsSelect(tags) {
+function createTagsSelect(tags, tagsSelected) {
+    let tagsHTML = '';
     for (let tag of tags) {
+        //option 1
         tagsHTML += `
-        <option value="${tag}">${tag}</option>`
+            <option value="${tag}`;
+        if (tagsSelected != null && tagsSelected.includes(tag)) {
+            tagsHTML += ` selected`
+        }
+        tagsHTML += `>${tag}</option>`
+        //option 2
+        let selected = '';
+        if (tagsSelected != null && tagsSelected.includes(tag)) {
+            selected = ' selected';
+        }
+        tagsHTML += `<option value="${tag}"${selected}>${tag}</option>`;
+        //option 3
+        if (tagsSelected != null && tagsSelected.includes(tag)) {
+            tagsHTML += `
+            <option value="${tag}" selected>${tag}</option>`
+        } else {
+            tagsHTML += `
+            <option value="${tag}">${tag}</option>`
+        }
     }
     document.getElementById('tags').innerHTML = tagsHTML;
 }
 
-createTagsSelect(tags);
+createTagsSelect(tags, null);
 
 function createPostEvent() {
     let form = document.getElementById('create-post-form');
     form.addEventListener('submit', async function (e) {
         e.preventDefault();
-        //bryt ut
         displayErrorMessage.innerHTML = '';
-        errorMessage = '';
-        errorMessages(title.value, author.value, content.value);
+        let errorMessage = errorMessages(title.value, author.value, content.value);
         if (errorMessage != '') {
             displayErrorMessage.innerHTML = errorMessage;
             return;
         }
 
-        // let errorMessage = '';
-        // if (isStringEmpty(title.value)) {
-        //     errorMessage += 'Title is required<br>';
-        // }
-        // if (isStringEmpty(author.value)) {
-        //     errorMessage += 'Author is required<br>';
-        // }
-        // if (isStringEmpty(content.value)) {
-        //     errorMessage += 'Content is required<br>';
-        // }
-
-        // if (errorMessage != '') {
-        //     displayErrorMessage.innerHTML = errorMessage;
-        //     return;
-        // }
-
-        //bryt ut
+        //bryt ut?
         let formData = new FormData(form);
         formDataObject = {
             "title": formData.get('title'),
@@ -75,7 +79,7 @@ function createPostEvent() {
             "tags": formData.getAll('tags')
         }
 
-
+        //Bryt ut till fetchfunktion?
         try {
             await fetch('http://localhost:5000/posts', {
                 method: 'POST',
