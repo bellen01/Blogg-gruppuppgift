0dda157a8ad394d41e8319a925459c2dc11a9fd7
Isabelle <isabelle.annie.karlsson@gmail.com>
2021-12-09 10:32:29 +0100

    Rewrote the validation function

--- blog-client-template/admin/js/create-post.js
+++ blog-client-template/admin/js/create-post.js
@@ -1,96 +1,103 @@
 window.onload = function () {
     createPostEvent();
-    let title = document.getElementById('title');
-    let author = document.getElementById('author');
-    let content = document.getElementById('content');
+    // let title = document.getElementById('title');
+    // let author = document.getElementById('author');
+    // let content = document.getElementById('content');
     let displayErrorMessage = document.getElementById('error-messages');
-    let tagsSelect = document.getElementById('tags');
-}
 
 
-//flytta till egen js som både jag och Linn kan använda?
-function isStringEmpty(text) {
-    return text.trim() === '';
-}
-function errorMessages(titleValue, authorValue, contentValue) {
+
+    //flytta till egen js som både jag och Linn kan använda?
+    function isStringEmpty(text) {
+        return text.trim() === '';
+    }
+
+    let errorMessageParameters = [
+        "Title",
+        "Author",
+        "Content",
+    ]
+
     let errorMessage = '';
-    if (isStringEmpty(titleValue)) {
-        errorMessage += 'Title is required<br>';
-    }
-    if (isStringEmpty(authorValue)) {
-        errorMessage += 'Author is required<br>';
-    }
-    if (isStringEmpty(contentValue)) {
-        errorMessage += 'Content is required<br>';
-    }
-    return errorMessage;
-}
-
-
-//flytta till egen js som både jag och Linn kan använda?
-let tags = ["Politics", "Satire", "Autobiography", "Lorem Gang"];
-function createTagsSelect(tags, tagsSelected) {
-    let tagsHTML = '';
-    for (let tag of tags) {
-        //option 1
-        tagsHTML += `
-            <option value="${tag}`;
-        if (tagsSelected != null && tagsSelected.includes(tag)) {
-            tagsHTML += ` selected`
-        }
-        tagsHTML += `>${tag}</option>`
-        //option 2
-        let selected = '';
-        if (tagsSelected != null && tagsSelected.includes(tag)) {
-            selected = ' selected';
-        }
-        tagsHTML += `<option value="${tag}"${selected}>${tag}</option>`;
-        //option 3
-        if (tagsSelected != null && tagsSelected.includes(tag)) {
-            tagsHTML += `
-            <option value="${tag}" selected>${tag}</option>`
-        } else {
-            tagsHTML += `
-            <option value="${tag}">${tag}</option>`
+    function getErrorMessages(parameter) {
+        let input = document.getElementById(parameter);
+        if (isStringEmpty(input.value)) {
+            errorMessage += parameter + ' is required!<br>';
         }
     }
-    document.getElementById('tags').innerHTML = tagsHTML;
-}
 
-createTagsSelect(tags, null);
 
-function createPostEvent() {
-    let form = document.getElementById('create-post-form');
-    form.addEventListener('submit', async function (e) {
-        e.preventDefault();
-        displayErrorMessage.innerHTML = '';
-        let errorMessage = errorMessages(title.value, author.value, content.value);
-        if (errorMessage != '') {
-            displayErrorMessage.innerHTML = errorMessage;
-            return;
+    //skriva om till class och objekt??
+    // function errorMessages(titleValue, authorValue, contentValue) {
+    //     let errorMessage = '';
+    //     if (isStringEmpty(titleValue)) {
+    //         errorMessage += 'Title is required<br>';
+    //     }
+    //     if (isStringEmpty(authorValue)) {
+    //         errorMessage += 'Author is required<br>';
+    //     }
+    //     if (isStringEmpty(contentValue)) {
+    //         errorMessage += 'Content is required<br>';
+    //     }
+    //     return errorMessage;
+    // }
+
+
+    //flytta till egen js som både jag och Linn kan använda?
+    let tags = ["Politics", "Satire", "Autobiography", "Lorem Gang"];
+    function createTagsSelect(tags, tagsSelected) {
+        let tagsHTML = '';
+        for (let tag of tags) {
+            let selected = '';
+            if (tagsSelected != null && tagsSelected.includes(tag)) {
+                selected = ' selected';
+            }
+            tagsHTML += `<option value="${tag}"${selected}>${tag}</option>`;
         }
+        document.getElementById('tags').innerHTML = tagsHTML;
+    }
 
-        //bryt ut?
-        let formData = new FormData(form);
-        formDataObject = {
-            "title": formData.get('title'),
-            "content": formData.get('content'),
-            "author": formData.get('author'),
-            "tags": formData.getAll('tags')
-        }
+    createTagsSelect(tags, null);
 
-        //Bryt ut till fetchfunktion?
-        try {
-            await fetch('http://localhost:5000/posts', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(formDataObject),
-            })
-            location.replace('index.html');
-        } catch (error) {
-            console.log(error);
-        }
-    });
+    function createPostEvent() {
+        let form = document.getElementById('create-post-form');
+        form.addEventListener('submit', async function (e) {
+            e.preventDefault();
+            displayErrorMessage.innerHTML = '';
+            errorMessage = '';
+
+            for (let parameter of errorMessageParameters) {
+                getErrorMessages(parameter);
+            }
+            // errorMessage = errorMessages(title.value, author.value, content.value);
+            // let errorMessage = errorMessages(title.value, author.value, content.value);
+            if (errorMessage != '') {
+                displayErrorMessage.innerHTML = errorMessage;
+                return;
+            }
+
+            //bryt ut?
+            let formData = new FormData(form);
+            formDataObject = {
+                "title": formData.get('title'),
+                "content": formData.get('content'),
+                "author": formData.get('author'),
+                "tags": formData.getAll('tags')
+            }
+
+            //Bryt ut till fetchfunktion?
+            try {
+                await fetch('http://localhost:5000/posts', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(formDataObject),
+                })
+                location.replace('index.html');
+            } catch (error) {
+                console.log(error);
+            }
+        });
+    }
 }
