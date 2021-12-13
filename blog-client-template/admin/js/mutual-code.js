aa51e04e06c066eeb8affbed646e7ef228a9a182
hannarimon <hannarimon1@gmail.com>
2021-12-13 15:49:26 +0100

    added tags and put date into mutual code

--- blog-client-template/admin/js/mutual-code.js
+++ blog-client-template/admin/js/mutual-code.js
@@ -46,3 +46,19 @@ function createTagsSelect(tags, tagsSelected) {
     }
     document.getElementById('tags').innerHTML = tagsHTML;
 }
+
+// Function for formatting the date of the blog to make it beautiful, courtesy of Isabelle.
+let dateAndTimeFunction = (date) => {
+    let dateAndTime = new Date(date);
+    let year = dateAndTime.getFullYear();
+    let month = dateAndTime.getMonth() + 1;
+    if (month < 10) {
+        month = `0${month}`;
+    }
+    let day = dateAndTime.getDate();
+    if (day < 10) {
+        day = `0${day}`;
+    }
+    let formattedDateAndTime = `${year}-${month}-${day} ${dateAndTime.getHours()}:${dateAndTime.getMinutes()}`;
+    return formattedDateAndTime;
+};
--- blog-client-template/js/posts.js
+++ blog-client-template/js/posts.js
@@ -2,21 +2,6 @@ window.onload = function () {
     getBlogPosts();
 };
 
-let dateAndTimeFunction = (date) => {
-    let dateAndTime = new Date(date);
-    let year = dateAndTime.getFullYear();
-    let month = dateAndTime.getMonth() + 1;
-    if (month < 10) {
-        month = `0${month}`;
-    }
-    let day = dateAndTime.getDate();
-    if (day < 10) {
-        day = `0${day}`;
-    }
-    let formattedDateAndTime = `${year}-${month}-${day} ${dateAndTime.getHours()}:${dateAndTime.getMinutes()}`;
-    return formattedDateAndTime;
-}
-
 async function getBlogPosts() {
     try {
         let response = await fetch(`http://localhost:5000/posts`);
@@ -28,6 +13,7 @@ async function getBlogPosts() {
             
             <h2>${blogPost.title}</h2>
             <p>${blogPost.author} <br> ${dateAndTime}</p>
+            <p>${blogPost.tags.join(", ")}</p>
             <p data-id="${blogPost._id}"> ${blogPost.content.substring(0, 100)}... <a href="post.html?id=${blogPost._id}">read more</a></p>
             <hr> 
             `;
