a625a937e46c8adcfaf59a27660723663182b680
Isabelle <isabelle.annie.karlsson@gmail.com>
2021-12-13 15:33:51 +0100

    Moved code from create-post to mutual-code

--- blog-client-template/admin/js/create-post.js
+++ blog-client-template/admin/js/create-post.js
@@ -52,51 +52,51 @@ async function postBlogPostFetchFunction(object) {
 
 //================================= Flytta till gemensam js-fil om vi får göra så ===========================
 //Kod för validering av fält och felmeddelanden. Kan användas för både create och update
-function isStringEmpty(text) {
-    return text.trim() === '';
-}
+// function isStringEmpty(text) {
+//     return text.trim() === '';
+// }
 
-let errorMessageParameters = [
-    "Title",
-    "Author",
-    "Content",
-]
+// let errorMessageParameters = [
+//     "Title",
+//     "Author",
+//     "Content",
+// ]
 
-function checkRequiredFields(parameter) {
-    let input = document.getElementById(parameter);
-    if (isStringEmpty(input.value)) {
-        return parameter + ' is required!<br>';
-    }
-    return '';
-}
+// function checkRequiredFields(parameter) {
+//     let input = document.getElementById(parameter);
+//     if (isStringEmpty(input.value)) {
+//         return parameter + ' is required!<br>';
+//     }
+//     return '';
+// }
 
-function generateErrorMessages() {
-    let errorMessage = '';
-    for (let parameter of errorMessageParameters) {
-        errorMessage += checkRequiredFields(parameter);
-    }
-    return errorMessage;
-}
+// function generateErrorMessages() {
+//     let errorMessage = '';
+//     for (let parameter of errorMessageParameters) {
+//         errorMessage += checkRequiredFields(parameter);
+//     }
+//     return errorMessage;
+// }
 
-//Kod för tags, kan användas för både create och update
-let tags = [
-    "Politics",
-    "Satire",
-    "Autobiography",
-    "Lorem Gang"
-]
+// //Kod för tags, kan användas för både create och update
+// let tags = [
+//     "Politics",
+//     "Satire",
+//     "Autobiography",
+//     "Lorem Gang"
+// ]
 
-function createTagsSelect(tags, tagsSelected) {
-    let tagsHTML = '';
-    for (let tag of tags) {
-        let selected = '';
-        if (tagsSelected != null && tagsSelected.includes(tag)) {
-            selected = ' selected';
-        }
-        tagsHTML += `<option value="${tag}"${selected}>${tag}</option>`;
-    }
-    document.getElementById('tags').innerHTML = tagsHTML;
-}
+// function createTagsSelect(tags, tagsSelected) {
+//     let tagsHTML = '';
+//     for (let tag of tags) {
+//         let selected = '';
+//         if (tagsSelected != null && tagsSelected.includes(tag)) {
+//             selected = ' selected';
+//         }
+//         tagsHTML += `<option value="${tag}"${selected}>${tag}</option>`;
+//     }
+//     document.getElementById('tags').innerHTML = tagsHTML;
+// }
 
 
 
--- blog-client-template/admin/js/mutual-code.js
+++ blog-client-template/admin/js/mutual-code.js
@@ -1,1 +1,48 @@
 //File for code and functions that we need to use on multiple pages
+
+//Kod för validering av fält och felmeddelanden. Kan användas för både create och update
+function isStringEmpty(text) {
+    return text.trim() === '';
+}
+
+let errorMessageParameters = [
+    "Title",
+    "Author",
+    "Content",
+]
+
+function checkRequiredFields(parameter) {
+    let input = document.getElementById(parameter);
+    if (isStringEmpty(input.value)) {
+        return parameter + ' is required!<br>';
+    }
+    return '';
+}
+
+function generateErrorMessages() {
+    let errorMessage = '';
+    for (let parameter of errorMessageParameters) {
+        errorMessage += checkRequiredFields(parameter);
+    }
+    return errorMessage;
+}
+
+//Kod för tags, kan användas för både create och update
+let tags = [
+    "Politics",
+    "Satire",
+    "Autobiography",
+    "Lorem Gang"
+]
+
+function createTagsSelect(tags, tagsSelected) {
+    let tagsHTML = '';
+    for (let tag of tags) {
+        let selected = '';
+        if (tagsSelected != null && tagsSelected.includes(tag)) {
+            selected = ' selected';
+        }
+        tagsHTML += `<option value="${tag}"${selected}>${tag}</option>`;
+    }
+    document.getElementById('tags').innerHTML = tagsHTML;
+}
